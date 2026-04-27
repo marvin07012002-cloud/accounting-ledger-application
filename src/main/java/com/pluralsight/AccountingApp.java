ce865cea63f83ef6d7b5355b01db813c7fa8ea00
marbin701 <marvin07012002@gmail.com>
2026-04-27 09:49:45 -0400

    Menu/Home screen created and also Transaction class created

--- src/main/java/com/pluralsight/AccountingApp.java
+++ src/main/java/com/pluralsight/AccountingApp.java
@@ -1,4 +1,46 @@
 package com.pluralsight;
 
+import java.util.Scanner;
+
 public class AccountingApp {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        while(true){
+            System.out.println("Home Screen");
+            System.out.println("D) Add Deposit");
+            System.out.println("P) Make a payment");
+            System.out.println("L) Ledger ");
+            System.out.println("X) Exit");
+
+            String choice = scanner.nextLine().trim().toUpperCase();
+
+            switch (choice){
+
+                case "D":
+                    addTransaction(scanner,true);
+                    break;
+                case "P":
+                    addTransaction(scanner,false);
+                    break;
+                case "L":
+                    showLedgerScreen(scanner);
+                    break;
+                case "X":
+                    return;
+                default:
+                    System.err.println("Invalid Choice");
+
+            }
+
+        }
+    }
+
+    private static void showLedgerScreen(Scanner scanner) {
+    }
+
+    private static void addTransaction(Scanner scanner, boolean b) {
+    }
+
+
 }
---
+++ src/main/java/com/pluralsight/Transaction.java
@@ -0,0 +1,14 @@
+package com.pluralsight;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+public class Transaction {
+    private LocalDate date;
+    private LocalTime time;
+    private  String description;
+    private String vendor;
+    private double amount;
+
+
+}
