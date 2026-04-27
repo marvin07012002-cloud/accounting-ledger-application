a84438b65693c7ae23c89deaaa4d0cdb4229c7dc
marbin701 <marvin07012002@gmail.com>
2026-04-27 11:50:32 -0400

    Create the addTransaction method and the with the required input with defensing coding. Also create the header method for an empty file, and in the transaction class I add the constructors, the getters and a method to write the transactions in order.

--- src/main/java/com/pluralsight/AccountingApp.java
+++ src/main/java/com/pluralsight/AccountingApp.java
@@ -1,12 +1,20 @@
 package com.pluralsight;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.time.LocalDate;
+import java.time.LocalTime;
 import java.util.Scanner;
 
 public class AccountingApp {
 
     public static void main(String[] args) {
+
+        createFileHeader();
         Scanner scanner = new Scanner(System.in);
-        while(true){
+        while (true) {
             System.out.println("Home Screen");
             System.out.println("D) Add Deposit");
             System.out.println("P) Make a payment");
@@ -15,13 +23,13 @@ public class AccountingApp {
 
             String choice = scanner.nextLine().trim().toUpperCase();
 
-            switch (choice){
+            switch (choice) {
 
                 case "D":
-                    addTransaction(scanner,true);
+                    addTransaction(scanner, true);
                     break;
                 case "P":
-                    addTransaction(scanner,false);
+                    addTransaction(scanner, false);
                     break;
                 case "L":
                     showLedgerScreen(scanner);
@@ -39,8 +47,92 @@ public class AccountingApp {
     private static void showLedgerScreen(Scanner scanner) {
     }
 
-    private static void addTransaction(Scanner scanner, boolean b) {
+    private static void addTransaction(Scanner scanner, boolean isDeposit) {
+
+        String description = requiredInput(scanner, "Description: ");
+        String vendor = requiredInput(scanner, "Vendor: ");
+
+        double amount;
+
+        while (true) {
+            System.out.println("Amount: ");
+            String amountInput = scanner.nextLine().trim();
+
+            try {
+                amount = Double.parseDouble(amountInput);
+
+                if (amount * 100 != Math.round(amount * 100)) {
+                    System.err.println("Only 2 decimal places allowed.");
+                    continue;
+                }
+                break;
+            } catch (NumberFormatException e) {
+                System.err.println("Please enter a valid number, like 25.50");
+            }
+
+        }
+
+
+        if (isDeposit) {
+            amount = Math.abs(amount);
+        } else {
+            amount = -Math.abs(amount);
+        }
+
+        LocalDate date = LocalDate.now();
+        LocalTime time = LocalTime.now().withNano(0);
+
+        Transaction transaction = new Transaction(date, time, description, vendor, amount);
+
+        try {
+            BufferedWriter writer = new BufferedWriter(new FileWriter("src/main/resources/transactions.csv", true));
+
+            writer.write(transaction.csvString());
+            writer.newLine();
+
+            writer.close();
+
+            System.out.println("Transaction saved!");
+
+
+        } catch (IOException e) {
+
+            System.err.println("There was a problem saving the transaction.");
+        }
+
+    }
+
+    private static String requiredInput(Scanner scanner, String prompt) {
+        while (true) {
+            System.out.println(prompt);
+            String input = scanner.nextLine().trim();
+
+            if (!input.isEmpty()) {
+                return input;
+            }
+            System.err.println(prompt + "cannot be blank.");
+        }
+    }
+
+    private static void createFileHeader(){
+        try{
+            File file = new File("src/main/resources/transactions.csv");
+
+            if(!file.exists() || file.length() == 0){
+
+                BufferedWriter writer = new BufferedWriter(new FileWriter(file));
+
+                writer.write("date|time|description|vendor|amount");
+                writer.newLine();
+                writer.close();
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
     }
 
 
-}
+
+
--- src/main/java/com/pluralsight/Transaction.java
+++ src/main/java/com/pluralsight/Transaction.java
@@ -6,9 +6,41 @@ import java.time.LocalTime;
 public class Transaction {
     private LocalDate date;
     private LocalTime time;
-    private  String description;
+    private String description;
     private String vendor;
     private double amount;
 
+    public LocalDate getDate() {
+        return date;
+    }
 
+    public LocalTime getTime() {
+        return time;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getVendor() {
+        return vendor;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public Transaction(LocalDate date, LocalTime time, String description, String vendor, double amount) {
+        this.date = date;
+        this.time = time;
+        this.description = description;
+        this.vendor = vendor;
+        this.amount = amount;
+    }
+
+    public String csvString() {
+        return date + "|" + time + "|" + description + "|" + vendor + "|" + amount;
+    }
 }
+
+
